8e0518c8165eff4c63f54d31c25089c6aff62817
Brandon Montalvo <79609464+brand0nM@users.noreply.github.com>
2022-03-26 13:09:13 -0600

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -12,9 +12,11 @@ total_votes = 0
 
 # Create empty containers
 candidate_options = []
-voties = []
-percenties = []
 candidate_votes = {}
+
+# Track the winning candidate, vote count, and percentage.
+winning_candidate = ""
+
 
 with open(file_to_load) as election_data:
 
@@ -36,31 +38,35 @@ with open(file_to_load) as election_data:
             candidate_options.append(candidate_name)
 
             #initialize candidate vote counter in our dictionary
-            candidate_votes[candidate_name] = 1 
+            candidate_votes[candidate_name] = 0 
 
         # Count amount of entries that aren't unique
         else:
             candidate_votes[candidate_name] += 1
 
-    # Loop over data set to print our results
-    for candidate_name in candidate_votes:
-        votes = candidate_votes[candidate_name]
+for candidate_name in candidate_votes:
+    # Retrieve vote count and percentage.
+    votes = candidate_votes[candidate_name]
+    vote_percentage = round(float(votes) / float(total_votes) * 100, 1)
+    # Print each candidate, their voter count, and percentage to the
+    # terminal.
+    print(f"{candidate_name}: {vote_percentage}% ({votes:,})\n")
+    winning_count = 0
+    winning_percentage = 0
+    # Determine winning vote count, winning percentage, and candidate.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_candidate = candidate_name
+        winning_percentage = vote_percentage
+# Print the winning candidates' results to the terminal.
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage}%\n"
+    f"-------------------------\n")
 
-        # Calculate the percentage of votes.
-        vote_percentage = round(float(votes) / float(total_votes) * 100, 1)
-        voties.append(votes)
-        percenties.append(vote_percentage)
-    
-    winning_count = max(voties)
-    winning_percentage = max(percenties)
-
-    for candidate_name in candidate_votes:
-        if (votes >= winning_count) and (vote_percentage >= winning_percentage):
-             # And, set the winning_candidate equal to the candidate's name.
-            winning_candidate = candidate_name
-            print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")    
-        else:
-            print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")    
+print(winning_candidate_summary)
 
 # Close the file
 election_data.close()
